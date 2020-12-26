f4ad4d29cf4a87fb1adbfc02592099c8adda19ec
rethc <reth@xtra.co.nz>
2020-12-27 00:04:48 +1300

    our story footer complete

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Title from '../Globals/Title'
 
 export default function info() {
     return (
         <section class="py-5">
             <div className="container">
+                <Title title="our story"></Title>
                 <div className="row">
                     <div className="col-10 col-sm-8 mx-auto text-center">
                         <p className="lead text-muted mb-5">
