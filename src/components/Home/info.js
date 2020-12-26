32d1c070ef0157ff6b30c09b9f689601ad96b91c
rethc <reth@xtra.co.nz>
2020-12-26 23:56:22 +1300

    about page

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -10,6 +10,9 @@ export default function info() {
                         <p className="lead text-muted mb-5">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
+                        <Link to="/about/">
+                            <button className="btn text-uppercase btn-yellow">About Page</button>
+                        </Link>
                     </div>
                 </div>
             </div>
