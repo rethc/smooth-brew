c6fdb8e5876757494ab7c17a8d31d45b37983f59
rethc <reth@xtra.co.nz>
2020-12-26 23:53:38 +1300

    placeholder for about info page

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -3,8 +3,16 @@ import { Link } from 'gatsby'
 
 export default function info() {
     return (
-        <div>
-
-        </div>
+        <section class="py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 col-sm-8 mx-auto text-center">
+                        <p className="lead text-muted mb-5">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section >
     )
 }
