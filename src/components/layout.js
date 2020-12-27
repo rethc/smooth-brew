5c5cb0068acccb8b930842cd9d52641aecf6b21f
rethc <reth@xtra.co.nz>
2020-12-27 23:32:32 +1300

    prettifer

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,8 +5,8 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./Globals/NavBar";
 import Footer from "./Globals/Footer";
