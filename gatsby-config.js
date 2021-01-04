50795434c43e3af10e0c363d3bb0e8a53ca0c2dd
rethc <reth@xtra.co.nz>
2021-01-04 16:53:26 +1300

    configure google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-MD51PKN93T",
+        trackingId: "UA-186451791-1",
       },
     },
     `gatsby-plugin-material-ui`,
